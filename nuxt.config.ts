a5e038ce695413ffe13d886c758f08eded41ca09
chernandezdesigner <chernandezdesigner@gmail.com>
2025-09-29 13:32:57 -0400

    deployed prototype 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes',
+      charset: 'utf-8'
+    }
+  }
 })
